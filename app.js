2cdab1713389891cbd4fb78da115334826b7b0fc
Sia <ttontia@gmail.com>
2022-01-04 18:27:23 +0900

    SAVE the Image. final.

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#000000";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#000000";
+ctx.strokeStyle = "#2c2c2c";
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; /* 라인 굵기 */
 
 let painting = false;
@@ -36,6 +46,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -49,8 +60,29 @@ function handleModeClick() {
    mode.innerText = "Fill";
  } else {
   filling = true;
-  mode.innerText = "Paint";
+  mode.innerText = "Paint";  
  }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+      ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+  }
+
+// 우클릭 방지
+/*
+function handleCM(event) {
+   event.preventDefault();
+ }
+ */
+
+function handleSaveClick() {
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
 }
 
 if (canvas) {
@@ -58,6 +90,9 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    // canvas.addEventListener("contextmenu", handleCM);
+
 }
 
 Array.from(colors).forEach(color => 
@@ -72,3 +107,6 @@ if (mode) {
     mode.addEventListener("click", handleModeClick);
 }
 
+if (saveBtn){
+  saveBtn.addEventListener("click", handleSaveClick);
+}
